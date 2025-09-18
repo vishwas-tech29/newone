45ce8a426bd6ff71fb50440c2c52cdedbeb2e1c6
vishwas-tech29 <rallapallivishwas76@gmail.com>
2025-09-18 13:06:27 +0530

    sdl;cm

--- package/src/app/components/scroll-to-top/index.tsx
+++ package/src/app/components/scroll-to-top/index.tsx
@@ -32,9 +32,7 @@ export default function ScrollToTop() {
   return (
     <div className="fixed bottom-8 right-8 z-999">
       <div className="flex items-center gap-2.5">
-        <Link href={"https://getnextjstemplates.com/products/avenue-pro-nextjs-travel-website-template"} target="_blank" className="hidden lg:block bg-purple_blue/80 text-sm hover:bg-purple_blue text-white px-4 py-3.5 leading-none rounded-lg font-medium text-nowrap">
-          Get This Template
-        </Link>
+        
         {isVisible && (
           <div
             onClick={scrollToTop}
